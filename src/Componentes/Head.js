679bed3de9e525b9ce28eebbe58005415619aea8
369oscarCastillo <oscalibe@gmail.com>
2022-09-05 19:09:17 -0500

    url fixed

--- src/Componentes/Head.js
+++ src/Componentes/Head.js
@@ -3,7 +3,7 @@ import "../Hojas-de-estilo/Head.css";
 
 export function Imagen() {
   return (
-    <a href="https://dhymining.com">
+    <a href="https://www.dhymining.com">
       <img className="logo" src={require(`../imagenes/logo.jpg`)} alt='logo' title="DHY Mining"/>;
     </a>
   );
